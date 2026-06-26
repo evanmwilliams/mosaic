9d0058204e6c69d908d83d7a0900076a73d64ee5
meerkat766 <malenacochrane@gmail.com>
2026-06-26 17:50:55 -0400

    Commenting out stuff to see if docker works

--- local-tests/test-SDDMM.cpp
+++ local-tests/test-SDDMM.cpp
@@ -3,26 +3,26 @@
 #include "taco/accelerator_notation/accel_interface.h"
 #include "taco/accelerator_interface/cblas_interface.h"
 
-using namespace taco;
+// using namespace taco;
 
-int main() {
-  const int I = 10, J = 10, K = 10;
-  Tensor<float> B("B", {I, K}, CSR);
-  Tensor<float> C("C", {I, J}, {Dense, Dense});
-  Tensor<float> D("D", {J, K}, {Dense, Dense});
-  Tensor<float> A("A", {I, K}, {Dense, Dense}, 0);
+// int main() {
+//   const int I = 10, J = 10, K = 10;
+//   Tensor<float> B("B", {I, K}, CSR);
+//   Tensor<float> C("C", {I, J}, {Dense, Dense});
+//   Tensor<float> D("D", {J, K}, {Dense, Dense});
+//   Tensor<float> A("A", {I, K}, {Dense, Dense}, 0);
 
-  IndexVar i("i"), j("j"), k("k");
-  A(i, k) = B(i, k) * sum(j, C(i, j) * D(j, k));
+//   IndexVar i("i"), j("j"), k("k");
+//   A(i, k) = B(i, k) * sum(j, C(i, j) * D(j, k));
 
-  A.registerAccelerator(new Saxpy());          
-  A.registerAccelerator(new Sdot());           
-  A.registerAccelerator(new MatrixMultiply());  
+//   A.registerAccelerator(new Saxpy());          
+//   A.registerAccelerator(new Sdot());           
+//   A.registerAccelerator(new MatrixMultiply());  
 
-  // Run the automatic mapper on the registered functions.
-  IndexStmt stmt = A.getAssignment();
-  if (isEinsumNotation(stmt)) stmt = makeReductionNotation(stmt);
-  stmt.autoAccelerate(stmt, A.getRegisteredAccelerators());
+//   // Run the automatic mapper on the registered functions.
+//   IndexStmt stmt = A.getAssignment();
+//   if (isEinsumNotation(stmt)) stmt = makeReductionNotation(stmt);
+//   stmt.autoAccelerate(stmt, A.getRegisteredAccelerators());
 
-  return 0;
-}
+//   return 0;
+// }
--- local-tests/test-tblis-add.cpp
+++ local-tests/test-tblis-add.cpp
@@ -4,27 +4,27 @@
 #include "taco/accelerator_interface/cblas_interface.h"
 #include "taco/accelerator_interface/tblis_interface.h"
 
-using namespace taco;
+// using namespace taco;
 
-int main() {
-  const int I = 10, J = 10, K = 10;
-  Tensor<float> B("B", {I, J, K}, {Dense, Dense, Dense});
-  Tensor<float> C("C", {I, J, K}, {Dense, Dense, Dense});
-  Tensor<float> A("A", {I, J, K}, {Dense, Dense, Dense}, 0);
+// int main() {
+//   const int I = 10, J = 10, K = 10;
+//   Tensor<float> B("B", {I, J, K}, {Dense, Dense, Dense});
+//   Tensor<float> C("C", {I, J, K}, {Dense, Dense, Dense});
+//   Tensor<float> A("A", {I, J, K}, {Dense, Dense, Dense}, 0);
 
-  IndexVar i("i"), j("j"), k("k");
-  A(i, j, k) = B(i,j,k) + C(i,j,k);
-  //A(i, k) = B(i, k) * sum(j, C(i, j) * D(j, k));
+//   IndexVar i("i"), j("j"), k("k");
+//   A(i, j, k) = B(i,j,k) + C(i,j,k);
+//   //A(i, k) = B(i, k) * sum(j, C(i, j) * D(j, k));
 
-  A.registerAccelerator(new Saxpy());          
-  A.registerAccelerator(new Sdot());           
-  A.registerAccelerator(new MatrixMultiply());  
-  A.registerAccelerator(new TblisPlus());
+//   A.registerAccelerator(new Saxpy());          
+//   A.registerAccelerator(new Sdot());           
+//   A.registerAccelerator(new MatrixMultiply());  
+//   A.registerAccelerator(new TblisPlus());
 
-  // Run the automatic mapper on the registered functions.
-  IndexStmt stmt = A.getAssignment();
-  if (isEinsumNotation(stmt)) stmt = makeReductionNotation(stmt);
-  stmt.autoAccelerate(stmt, A.getRegisteredAccelerators());
+//   // Run the automatic mapper on the registered functions.
+//   IndexStmt stmt = A.getAssignment();
+//   if (isEinsumNotation(stmt)) stmt = makeReductionNotation(stmt);
+//   stmt.autoAccelerate(stmt, A.getRegisteredAccelerators());
 
-  return 0;
-}
+//   return 0;
+// }
